f188e3e09adafccd79ea7a5478d538535de70628
aryanarvii <aryanarvi78@gmail.com>
2025-10-08 20:53:48 +0530

    dynamic port set

--- Backend/server.js
+++ Backend/server.js
@@ -3,10 +3,10 @@
 
 const app = require('./src/app.js')
 const connectDB = require('./src/db/db.js')
-
+const PORT = process.env.PORT || 3000;
 
 connectDB()
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on 3000")
 })
