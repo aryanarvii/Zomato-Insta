75f6afff1cd79ef6f8c3d91a5ee29630494dc5ca
aryanarvii <aryanarvi78@gmail.com>
2025-10-09 18:50:11 +0530

    frontend integrate

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
     credentials: true,
 }))
 
